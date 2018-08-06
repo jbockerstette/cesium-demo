df3648b1f67df36743977558d0bc2121f8b8af78
jbockerstette <jim.bockerstette@gmail.com>
2018-08-06 13:35:26 -0700

    Update Webpack prod config to use the Cesium DLL bundle

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -230,6 +230,10 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DllReferencePlugin({
+      context: paths.cesiumSourceFolder,
+      manifest: require(path.join(paths.app, 'distdll/cesiumDLL-manifest.json'))
+    }),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
