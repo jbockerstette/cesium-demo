1b8563bf4c29336c22f3a60acd28612227089792
jbockerstette <jim.bockerstette@gmail.com>
2018-08-07 11:26:06 -0700

    Rework styling to show some text over the globe

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React from 'react';
 import CesiumGlobe from './cesium/CesiumGlobe';
 
 function App() {
+  const containerStyle = {
+    width: '100%',
+    height: '100%',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    position: 'fixed'
+  };
   return (
-    <div>
-      <CesiumGlobe />
+    <div style={containerStyle}>
+      <CesiumGlobe /> <CesiumGlobe />
+      <div style={{ position: 'fixed', top: 0 }}>
+        <div style={{ color: 'white', fontSize: 40 }}>Text Over the Globe</div>
+      </div>
     </div>
   );
 }
