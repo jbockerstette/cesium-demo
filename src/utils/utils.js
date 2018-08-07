7df2464c247a307be4800b748d551afd2e9ef8ce
jbockerstette <jim.bockerstette@gmail.com>
2018-08-07 11:51:26 -0700

    Added a generic Billboard and a project contents components

---
+++ src/utils/utils.js
@@ -0,0 +1,18 @@
+export default function shallowEqual(objA, objB) {
+  if (objA === objB) {
+    return true;
+  }
+  const aKeys = Object.keys(objA);
+  const bKeys = Object.keys(objB);
+  const len = aKeys.length;
+  if (bKeys.length !== len) {
+    return false;
+  }
+  for (let i = 0; i < len; i += 1) {
+    const key = aKeys[i];
+    if (objA[key] !== objB[key]) {
+      return false;
+    }
+  }
+  return true;
+}
